6d4f83ef9f03e06eae425f19ac085f141b78c857
alon6699 <allonmaliii66@gmail.com>
2019-09-02 14:28:38 +0300

    create task handler for handling client tasks

--- src/main/java/com/server/Main.java
+++ src/main/java/com/server/Main.java
@@ -2,16 +2,29 @@ package com.server;
  
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
- 
+import java.lang.reflect.Method;
+import java.util.HashMap;
+
 import org.glassfish.tyrus.server.Server;
- 
+
 public class Main {
- 
+    public static final HashMap<String, Method> taskList = new HashMap<>();
     public static void main(String[] args) {
+        setTaskHandler();
         runServer();
     }
+
+    private static void setTaskHandler() {
+        Method[] classMethods = TaskHandler.class.getDeclaredMethods();
+        for (Method method : classMethods) { 
+            TaskAnnotation annotation = method.getAnnotation(TaskAnnotation.class);
+            if (annotation != null) {                
+                TaskHandler.taskList.put(annotation.value(), method);
+            }
+        }
+    }
  
-    public static void runServer() {
+    private static void runServer() {
         Server server = new Server("localhost", 8025, "/websockets", WebSocketServer.class);
         try {
             server.start();
